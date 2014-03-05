00b698f12e76f562005c559d272d0666f161c6f7
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-03-05 15:58:41 +0100

    adding fonts and improving compile script

--- compile.rb
+++ compile.rb
@@ -1,5 +1,13 @@
 # compile everything into the index.html file
+# for max efficiency, compile right into the clipboard for pasting into tumblr:
+# ruby compile.rb && cat index.html | pbcopy
 
 `lessc -x styles.less styles.css`
-cmd = "sed 's/!__COMPILE_CSS/" + `cat styles.css`.strip + "/g' index.html.tumblr > index.html"
-exec(cmd)
+styles = `cat styles.css`.strip
+styles = styles.gsub(/"/, "'")
+styles = styles.gsub(/\\/, "\\") # "\" => "\\"
+styles = styles.gsub("/", '\\/') # "/" => "\/"
+styles = styles.gsub(/\&/, "\&") # "\" => "\\"
+cmd = "sed \"s/!__COMPILE_CSS/" + styles + "/g\" index.html.tumblr > index.html"
+system(cmd)
+puts "done."
